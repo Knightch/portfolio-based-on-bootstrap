27685f31442cc5ed2eae38ee794de159d6feca8e
knightch <amankumarpandey2397@gmail.com>
2022-08-29 21:32:51 +0530

    create intro section

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const lightbox = GLightbox({
+    'href': 'https://www.youtube.com/watch?v=8h12ccQgnVU&list=RDDXCd7Moy3to&index=4',
+    'type': 'video',
+    'source': 'youtube', //vimeo, youtube or local
+    'width': 900,
+    'autoplays': true
+});
